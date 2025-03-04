2c21c4f923f81caa42e67ac2fdd9ea05054d82e3
xComputer-x <adam.kiliszek.10@gmail.com>
2025-03-04 13:56:30 +0100

    Dodano formatowanie zmiennych (na razie tylko dla globalnych, ale pewnie zadziała też dla zwykłych (oby))
    Poza tym, debugger wysyła komendy do gdb pozwalające mu obliczyć
    aktualną funkcje, typ aktualnej funkcji, aktualną linie i typy
    parametrów aktualnej funkcji (to też formatuje z odpowiedzi gdb).

--- a.cpp
+++ a.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int e()
+{
+	return 0;
+}
+
+void c(void* d)
+{
+	return;
+}
+
+int a, b;
+
 int main()
 {
     int a, b;
--- src/gdb_manager.py
+++ src/gdb_manager.py
@@ -1,16 +1,15 @@
 import os
+import re
 import time
 import pexpect
-from uuid import uuid4
-from pprint import pprint
-from typing import Optional
+from typing import Optional, Any
 from pygdbmi.gdbmiparser import parse_response
 
 import docker_response_status as DckStatus
 from compiler_manager import Compiler
 from docker_manager import DockerManager
 from logger import Logger
-from server import DEBUGGER_MEMORY_LIMIT_MB, EXPECT_VALUES_AFTER_GDB_COMMAND
+from server import DEBUG_DIR, DEBUGGER_MEMORY_LIMIT_MB, EXPECT_VALUES_AFTER_GDB_COMMAND
 
 
 '''
@@ -46,7 +45,6 @@ class GDBDebugger:
 			"set substitute-path /home/adam/repos/InformejtycyDebugger/./received /app",
 			"break *main",
 			"run",
-			"frame"
 		]
 
 		self.compiled_file_name = ""
@@ -62,7 +60,76 @@ class GDBDebugger:
 		'''
 		self.last_ping_time = time.time()
 	
-	def send_command(self, command: str) -> tuple[str, str]:
+	def get_formatted_gdb_output(self, whole_output: bool = False) -> list[dict[str: Any]]:
+		outputs = []
+		for line in self.process.before.split('\n'):
+			output = parse_response(line)
+			if not whole_output and output["type"] == "console":
+				outputs.append(output)
+			elif whole_output:
+				outputs.append(output)
+		return outputs
+	
+	def get_server_output_data(self) -> dict[str: Any]:
+		frame_output = self.send_command("frame")[1][-2:]
+		current_function = frame_output[0]["payload"].split(' ')[2]
+		current_line = int(frame_output[1]["payload"].split('\t')[0]) 
+
+		whatis_output = self.send_command(f"whatis {current_function}")[1]
+		current_function_return_type = whatis_output[0]["payload"].split(' ')[2]
+		current_function_params_types = whatis_output[0]["payload"].split(' ')[3][:-1]
+		current_function_params_types = current_function_params_types if current_function_params_types != "(void)" else "()"
+
+		self.logger.spam(f"Global variables: {self.get_global_variables()}", self.get_server_output_data)
+
+		self.logger.info(f"Current function: {current_function}", self.get_server_output_data)
+		self.logger.info(f"Current function's return type: {current_function_return_type}", self.get_server_output_data)
+		self.logger.info(f"Current function's parameters types: {current_function_params_types}", self.get_server_output_data)
+		self.logger.info(f"Current line: {current_line}", self.get_server_output_data)
+
+	def get_global_variables(self) -> list[dict[str: Any]]:
+		variables_output = self.send_command("info variables")[1]
+		global_variables = []
+
+		for i in range(len(variables_output)):
+			if variables_output[i]["payload"] == "\nNon-debugging symbols:\n":
+				break
+			
+			if variables_output[i]["payload"] == "\nFile " and variables_output[i+1]["payload"].startswith(DEBUG_DIR):
+				k = i+1
+
+				while not variables_output[k]["payload"].startswith("\nFile") and not variables_output[k]["payload"] == "\nNon-debugging symbols:\n":
+					if variables_output[k]["payload"].startswith(DEBUG_DIR):
+						k+=1
+						continue
+					global_variables.append(self.format_a_variable(variables_output[k]["payload"]))
+					k+=1
+				break
+
+		return global_variables
+
+	def format_a_variable(self, gdb_output) -> dict[str: Any]:
+		try:
+			variable_match = re.match(r"(.+?)\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*((?:\[[^\]]*\])*)$", re.sub(r"\s*\*", "* ", gdb_output[:-2].split('\t')[1]))
+			
+			variable_type = variable_match.group(1)
+			variable_name = variable_match.group(2)
+
+			try:
+				amount_of_values = [int(match.group(1)) for match in re.finditer(r'\[(\d+)\]', variable_match.group(3))] or 1
+			except:
+				amount_of_values = 1
+			
+			p_output = self.send_command(f"p {variable_name}")[1]
+			variable_value = p_output[0]["payload"].split('=')[1][1:-1]
+
+			return {"variable_supported": True, "variable_type": variable_type, "variable_name": variable_name, "variable_value": variable_value, "amount_of_values": amount_of_values}
+		except Exception as e:
+			self.logger.warn(f"Variable {gdb_output} couln't be displayed... | {e.__class__.__name__}: {e}", self.format_a_variable)
+		
+		return {"variable_supported": False, "variable_type": "", "variable_name": "", "variable_value": "", "amount_of_values": ""}
+
+	def send_command(self, command: str) -> tuple[str, list[dict[str: Any]]]:
 		'''
 		Executes gdb command.
 		:param command: command, to be executed
@@ -82,7 +149,7 @@ class GDBDebugger:
 		except Exception as e:
 			self.logger.alert(f"Couldn't send {command} command to gdb process | {e.__class__.__name__}: {e}", self.send_command)
 
-		return (which_response, self.process.before)
+		return (which_response, self.get_formatted_gdb_output())
 
 	def run(self, input_: str) -> tuple[int, bytes]:
 		'''
@@ -122,6 +189,8 @@ class GDBDebugger:
 		self.logger.debug(f"Sending initalizing commands", self.run)
 		for command in self.gdb_init_input:
 			self.logger.spam(self.send_command(command)[1], self.run)
+		
+		self.get_server_output_data()
 
 		return (0, bytes())
 
